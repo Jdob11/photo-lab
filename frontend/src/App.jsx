b19f34f09fda078b5b81e8fb3cdccc64b5b2120f
Jdob11 <jbdobson11@gmail.com>
2024-05-23 16:54:48 -0700

    refactor: Use an array to display 3 PhotoListItem components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,10 +16,51 @@ const sampleDataForPhotoListItem = {
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const photos = [
+    {
+      id: "1",
+      location: {
+        city: "Montreal",
+        country: "Canada",
+      },
+      imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+      username: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+    {
+      id: "2",
+      location: {
+        city: "Montreal",
+        country: "Canada",
+      },
+      imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+      username: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+    {
+      id: "3",
+      location: {
+        city: "Montreal",
+        country: "Canada",
+      },
+      imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+      username: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    }
+  ]
 
   return (
     <div className="App">
-      <PhotoListItem photo={sampleDataForPhotoListItem}/>
+      {photos.map(photo => (
+        <PhotoListItem
+          key={photo.id}
+          imageSource={photo.imageSource}
+          city={photo.location.city}
+          country={photo.location.country}
+          username={photo.username}
+          profile={photo.profile}
+        />
+      ))}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
-  const {photo} = props;
+  const { imageSource, city, country, username, profile } = props;
   return (
     <article className="photo-list__item">
-      <img className="photo-list__image" src={photo.imageSource}/>
+      <img className="photo-list__image" src={imageSource}/>
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={photo.profile}/>
+        <img className="photo-list__user-profile" src={profile}/>
         <div className="photo-list__user-info">
-          {photo.username}
-          <div className="photo-list__user-location">{photo.location.city}, {photo.location.country}</div>
+          {username}
+          <div className="photo-list__user-location">{city}, {country}</div>
         </div>
       </div>
     </article>
