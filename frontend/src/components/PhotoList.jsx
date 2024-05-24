c5302359895c067e142b7dd1e21490ff8a919aec
Jdob11 <jbdobson11@gmail.com>
2024-05-24 12:21:31 -0700

    chore: Move .map functions into variable and put variable into return, instead of having .map function inline

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -5,18 +5,20 @@ import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
   const { photos } = props;
+  const photosArray = photos.map(({ id, urls: { full, regular}, location: { city, country }, user:{username, name, profile} }) => (
+    <PhotoListItem
+      key={id}
+      imageSource={regular}
+      city={city}
+      country={country}
+      name={name}
+      profile={profile}
+    />
+  ))
+
   return (
     <ul className="photo-list">
-      {photos.map(({ id, urls: { full, regular}, location: { city, country }, user:{username, name, profile} }) => (
-        <PhotoListItem
-          key={id}
-          imageSource={regular}
-          city={city}
-          country={country}
-          name={name}
-          profile={profile}
-        />
-      ))}
+      {photosArray}
     </ul>
   );
 };
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -5,14 +5,15 @@ import TopicListItem from "./TopicListItem";
 
 const TopicList = (props) => {
   const {topics} = props;
+  const topicsArray = topics.map(({ id, title }) => (
+    <TopicListItem
+      key={id}
+      title={title}
+    />
+  ))
   return (
     <div className="top-nav-bar__topic-list">
-      {topics.map(({ id, title}) => (
-        <TopicListItem
-          key={id}
-          title={title}
-        />
-      ))}
+      {topicsArray}
     </div>
   );
 };
