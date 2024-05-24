5c5aaab841076103d5a967b2a5af4c02b86afa5c
Jdob11 <jbdobson11@gmail.com>
2024-05-24 10:31:57 -0700

    refactor: Change props error handling to use PropTypes

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,26 +2,11 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
+import PropTypes from 'prop-types';
 
 const PhotoListItem = (props) => {
   const { imageSource, city, country, username, profile } = props;
   
-  let missingProps = '';
-
-  if (!imageSource) missingProps += 'imageSource, ';
-  if (!city) missingProps += 'city, ';
-  if (!country) missingProps += 'country, ';
-  if (!username) missingProps += 'username, ';
-  if (!profile) missingProps += 'profile, ';
-
-  if (missingProps) {
-    missingProps = missingProps.slice(0, -2);
-    return (
-      <article className="photo-list__item">
-        Error loading photo, the following props are missing: {missingProps}
-      </article>
-    );
-  }
   return (
     <article className="photo-list__item">
       <PhotoFavButton />
@@ -35,6 +20,14 @@ const PhotoListItem = (props) => {
       </div>
     </article>
   )
+};
+
+PhotoListItem.propTypes = {
+  imageSource: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  profile: PropTypes.string.isRequired
 };
 
 export default PhotoListItem;
