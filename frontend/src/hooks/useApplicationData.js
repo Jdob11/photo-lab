f4fb53727335ce85dad6aabe1720dfcbff276f59
Jdob11 <jbdobson11@gmail.com>
2024-05-27 12:40:43 -0700

    refactor: Change hook from useState to useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,44 +1,76 @@
-import { useState, useMemo } from 'react';
+import { useReducer, useMemo } from 'react';
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  OPEN_MODAL_WITH_PHOTO: 'OPEN_MODAL_WITH_PHOTO',
+  CLOSE_MODAL: 'CLOSE_MODAL'
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.OPEN_MODAL_WITH_PHOTO:
+      return {
+        ...state,
+        selectedPhoto: action.payload,
+      };
+    case ACTIONS.CLOSE_MODAL:
+      return {
+        ...state,
+        selectedPhoto: null,
+      };
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      };
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      return {
+        ...state,
+        favorites: state.favorites.filter(photo => photo.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  favorites: [],
+  selectedPhoto: null,
+};
 
 const useApplicationData = () => {
-  const [favorites, setFavorites] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleFavorite = (photo) => {
     if (!photo || !photo.id) return;
-    setFavorites((prevFavorites) => {
-      const isFavorite = prevFavorites.some((favPhoto) => favPhoto.id === photo.id);
-
-      if (isFavorite) {
-        return prevFavorites.filter((favPhoto) => favPhoto.id !== photo.id);
-      } else {
-        return [...prevFavorites, photo];
-      }
-    });
+    const isFavorite = state.favorites.some(favPhoto => favPhoto.id === photo.id);
+    if (isFavorite) {
+      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: photo });
+    } else {
+      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: photo });
+    }
   };
 
   const isFavPhotoExist = useMemo(() => {
-    return favorites.length > 0;
-  }, [favorites]);
-
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
+    return state.favorites.length > 0;
+  }, [state.favorites]);
 
   const openModalWithPhoto = (photo) => {
-    setSelectedPhoto(photo);
+    dispatch({ type: ACTIONS.OPEN_MODAL_WITH_PHOTO, payload: photo });
   };
 
   const closeModal = () => {
-    setSelectedPhoto(null);
+    dispatch({ type: ACTIONS.CLOSE_MODAL });
   };
 
   return {
-    favorites,
+    ...state,
     toggleFavorite,
     isFavPhotoExist,
-    selectedPhoto,
     openModalWithPhoto,
     closeModal,
   };
 };
 
 export default useApplicationData;
-
